1706073b9b348a1f24e91fc69c406b5b954a08c4
Tanner Chell <tanner.chell@gmail.com>
2019-01-04 16:24:35 -0700

    fix static path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve build if in production
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('client/build/'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
