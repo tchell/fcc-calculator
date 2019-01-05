b724bec5b8f033f5f0c705c38fc1bac163f3c8de
Tanner Chell <tanner.chell@gmail.com>
2019-01-04 17:18:52 -0700

    fix port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 // Serve build if in production
