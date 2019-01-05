8df4627853ab2f1da04e7188fcb756da524a92f3
Tanner Chell <tanner.chell@gmail.com>
2019-01-04 17:11:54 -0700

    fix build config

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 // Serve build if in production
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build/'));
+    app.use(express.static(__dirname + '/client/build/'));
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
