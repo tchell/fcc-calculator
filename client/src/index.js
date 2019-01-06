e374293b45248bafc64d749bef4f7a42d70440ba
Tanner Chell <tanner.chell@gmail.com>
2019-01-05 17:16:49 -0700

    add footer

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,9 @@ import store from './store';
 ReactDOM.render(
     <Provider store={store}>
         <App />
+        <footer>
+            by <a href="https://github.com/tchell">Tanner Chell</a> | <a href="https://github.com/tchell/fcc-calculator">view the code</a>
+        </footer>
     </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
