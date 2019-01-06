18a8c75f563158a357ac065c964092826866f4a3
Tanner Chell <tanner.chell@gmail.com>
2019-01-05 21:01:28 -0700

    fixed links

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(
     <Provider store={store}>
         <App />
         <footer>
-            by <a href="https://github.com/tchell">Tanner Chell</a> | <a href="https://github.com/tchell/fcc-calculator">view the code</a>
+            by <a href="https://github.com/tchell" target="_blank">Tanner Chell</a> | <a href="https://github.com/tchell/fcc-calculator" target="_blank">view the code</a>
         </footer>
     </Provider>, document.getElementById('root'));
 
